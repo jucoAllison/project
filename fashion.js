3d549543a07513d62b5306f290bf05b1566c85dd
jucoAllison <jucoallison@gmail.com>
2019-08-22 07:27:13 -0400

    first commit after installing packages like express, helmet, mongoose etc

---
+++ fashion.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const helmet = require('helmet');
+// https://github.com/jucoAllison/project.git
+const cors = require('cors');
+const mongoose = require('mongoose');
+// const  = require('');
+const app = express()
+
+app.listen(2019, console.log("app started at port 2019"))
